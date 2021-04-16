b85abb01c5ec8a6399ab6c6e28f036550ee4a2f2
Melroy Caeiro <71640510+MelroyCaeiro@users.noreply.github.com>
2021-04-16 12:48:57 +0800

    Create Gateway.ino

---
+++ Gateway.ino
@@ -0,0 +1,240 @@
+#include <Arduino.h>
+#include <SX126x-Arduino.h>
+#include <SPI.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <FirebaseESP32.h>
+#include <string>
+
+#define FIREBASE_HOST "https://esp32-01-5f0aa-default-rtdb.firebaseio.com/"
+#define FIREBASE_AUTH "jxigena1L1I8Ij0Hx8L1qn8poWTTi30RauojCOJa"
+#define WIFI_SSID "Gilroy'sWifi@unifi"
+#define WIFI_PASSWORD "matilda25"
+
+FirebaseData firebaseData;
+FirebaseJson json;
+
+int distance = 103;
+
+//----DATA
+long temp=0;
+long ambient=0;
+
+//----LORA
+hw_config hwConfig;
+
+#ifdef ESP32
+// ESP32 - SX126x pin configuration
+int PIN_LORA_RESET = 4;  // LORA RESET
+int PIN_LORA_DIO_1 = 21; // LORA DIO_1
+int PIN_LORA_BUSY = 22;  // LORA SPI BUSY
+int PIN_LORA_NSS = 5;	// LORA SPI CS
+int PIN_LORA_SCLK = 18;  // LORA SPI CLK
+int PIN_LORA_MISO = 19;  // LORA SPI MISO
+int PIN_LORA_MOSI = 23;  // LORA SPI MOSI
+int RADIO_TXEN = -1;	 // LORA ANTENNA TX ENABLE
+int RADIO_RXEN = -1;	 // LORA ANTENNA RX ENABLE
+#endif
+
+// Function declarations
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr);
+void OnRxError(void);
+
+// Check if the board has an LED port defined
+uint8_t led = 2;
+
+// Define LoRa parameters
+#define RF_FREQUENCY 868000000  // Hz
+#define TX_OUTPUT_POWER 22		// dBm
+#define LORA_BANDWIDTH 0		// [0: 125 kHz, 1: 250 kHz, 2: 500 kHz, 3: Reserved]
+#define LORA_SPREADING_FACTOR 7 // [SF7..SF12]
+#define LORA_CODINGRATE 1		// [1: 4/5, 2: 4/6,  3: 4/7,  4: 4/8]
+#define LORA_PREAMBLE_LENGTH 8  // Same for Tx and Rx
+#define LORA_SYMBOL_TIMEOUT 5   // Symbols
+#define LORA_FIX_LENGTH_PAYLOAD_ON false
+#define LORA_IQ_INVERSION_ON false
+#define RX_TIMEOUT_VALUE 30000
+#define TX_TIMEOUT_VALUE 3000
+
+#define BUFFER_SIZE 64 // Define the payload size here
+
+static RadioEvents_t RadioEvents;
+static uint16_t BufferSize = BUFFER_SIZE;
+static uint8_t RcvBuffer[BUFFER_SIZE];
+time_t timeToSend;
+int recv_payload1, recv_payload2, recv_payload3, recv_payload4;
+
+void setup()
+{
+
+    WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Serial.print("Connecting to Wi-Fi");
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  Serial.println();
+  Serial.print("Connected with IP: ");
+  Serial.println(WiFi.localIP());
+  Serial.println();
+ 
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.reconnectWiFi(true);
+
+  //Set database read timeout to 1 minute (max 15 minutes)
+  Firebase.setReadTimeout(firebaseData, 1000 * 60);
+  //tiny, small, medium, large and unlimited.
+  //Size and its write timeout e.g. tiny (1s), small (10s), medium (30s) and large (60s).
+  Firebase.setwriteSizeLimit(firebaseData, "tiny");
+
+//----LED
+
+  pinMode(led, OUTPUT);
+  digitalWrite(led, LOW);
+
+//----LROA
+  
+	// Define the HW configuration between MCU and SX126x
+	hwConfig.CHIP_TYPE = SX1261_CHIP;		  // Example uses an eByte E22 module with an SX1262
+	hwConfig.PIN_LORA_RESET = PIN_LORA_RESET; // LORA RESET
+	hwConfig.PIN_LORA_NSS = PIN_LORA_NSS;	 // LORA SPI CS
+	hwConfig.PIN_LORA_SCLK = PIN_LORA_SCLK;   // LORA SPI CLK
+	hwConfig.PIN_LORA_MISO = PIN_LORA_MISO;   // LORA SPI MISO
+	hwConfig.PIN_LORA_DIO_1 = PIN_LORA_DIO_1; // LORA DIO_1
+	hwConfig.PIN_LORA_BUSY = PIN_LORA_BUSY;   // LORA SPI BUSY
+	hwConfig.PIN_LORA_MOSI = PIN_LORA_MOSI;   // LORA SPI MOSI
+	hwConfig.RADIO_TXEN = RADIO_TXEN;		  // LORA ANTENNA TX ENABLE
+	hwConfig.RADIO_RXEN = RADIO_RXEN;		  // LORA ANTENNA RX ENABLE
+	hwConfig.USE_DIO2_ANT_SWITCH = false;	  // Example uses an CircuitRocks Alora RFM1262 which uses DIO2 pins as antenna control
+	hwConfig.USE_DIO3_TCXO = false;			  // Example uses an CircuitRocks Alora RFM1262 which uses DIO3 to control oscillator voltage
+	hwConfig.USE_DIO3_ANT_SWITCH = false;	 // Only Insight ISP4520 module uses DIO3 as antenna control
+
+	// Initialize Serial for debug output
+	Serial.begin(115200);
+
+	Serial.println("=====================================");
+	Serial.println("SX1261 Gateway");
+	Serial.println("=====================================");
+
+#ifdef ESP32
+	Serial.println("MCU Espressif ESP32");
+#endif
+
+	uint8_t deviceId[8];
+
+	BoardGetUniqueId(deviceId);
+	Serial.printf("BoardId: %02X-%02X-%02X-%02X-%02X-%02X-%02X-%02X\n",
+				  deviceId[7],
+				  deviceId[6],
+				  deviceId[5],
+				  deviceId[4],
+				  deviceId[3],
+				  deviceId[2],
+				  deviceId[1],
+				  deviceId[0]);
+
+	// Initialize the LoRa chip
+	Serial.println("Starting lora_hardware_init");
+	lora_hardware_init(hwConfig);
+
+	// Initialize the Radio callbacks
+	RadioEvents.RxDone = OnRxDone;
+	RadioEvents.RxError = OnRxError;
+
+	// Initialize the Radio
+	Radio.Init(&RadioEvents);
+
+	// Set Radio channel
+	Radio.SetChannel(RF_FREQUENCY);
+
+	// Set Radio TX configuration
+	Radio.SetTxConfig(MODEM_LORA, TX_OUTPUT_POWER, 0, LORA_BANDWIDTH,
+					  LORA_SPREADING_FACTOR, LORA_CODINGRATE,
+					  LORA_PREAMBLE_LENGTH, LORA_FIX_LENGTH_PAYLOAD_ON,
+					  true, 0, 0, LORA_IQ_INVERSION_ON, TX_TIMEOUT_VALUE);
+
+	// Set Radio RX configuration
+	Radio.SetRxConfig(MODEM_LORA, LORA_BANDWIDTH, LORA_SPREADING_FACTOR,
+					  LORA_CODINGRATE, 0, LORA_PREAMBLE_LENGTH,
+					  LORA_SYMBOL_TIMEOUT, LORA_FIX_LENGTH_PAYLOAD_ON,
+					  0, true, 0, 0, LORA_IQ_INVERSION_ON, true);
+
+	// Start LoRa
+	Serial.println("Starting Radio.Rx");
+	Radio.Rx(RX_TIMEOUT_VALUE);
+
+	timeToSend = millis();
+}
+
+void loop()
+{
+  
+	// Handle Radio events
+	Radio.IrqProcess();
+
+
+	// We are on FreeRTOS, give other tasks a chance to run
+	delay(100);
+	yield();
+	
+	json.set("/Sensor1", recv_payload1);
+  json.set("/Sensor2", recv_payload2);
+  json.set("/Sensor3", recv_payload3);
+  json.set("/Sensor4", recv_payload4);
+  Firebase.updateNode(firebaseData,"/Sensors",json);
+}
+
+
+/**@brief Function to be executed on Radio Rx Done event
+ */
+void OnRxDone(uint8_t *payload, uint16_t size, int16_t rssi, int8_t snr)
+{
+	Serial.println("OnRxDone");
+  digitalWrite(led, HIGH);
+	delay(250);
+  digitalWrite(led, LOW);
+	BufferSize = size;
+	memcpy(RcvBuffer, payload, BufferSize);
+
+	Serial.printf("RssiValue=%d dBm, SnrValue=%d\n", rssi, snr);
+
+//	for (int idx = 0; idx < size; idx++)
+//	{
+//		Serial.printf("%02X ", RcvBuffer[idx]);
+//	}
+//	Serial.println("");
+
+	if (BufferSize > 0)
+	{
+     for(int x=0; x<4; x++)
+      Serial.print(RcvBuffer[x]);
+      
+
+     Serial.println("");
+     Serial.println("------------------------------------");
+ 
+     //recv_payload = String(( char *) payload);
+     recv_payload1 = RcvBuffer[1];
+     recv_payload2 = RcvBuffer[2];
+     recv_payload3 = RcvBuffer[3];
+     recv_payload4 = RcvBuffer[4];
+     Serial.println(recv_payload1);
+     Serial.println(recv_payload2);
+     Serial.println(recv_payload3);
+     Serial.println(recv_payload4);
+     
+	}
+
+}
+
+
+/**@brief Function to be executed on Radio Rx Error event
+ */
+void OnRxError(void)
+{
+	Serial.println("OnRxError");
+	digitalWrite(led, LOW);
+	Radio.Rx(RX_TIMEOUT_VALUE);
+}
